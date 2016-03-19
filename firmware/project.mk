d3cc5ced35174a7d65864e850015ab385d185d63
Joe Ferner <joe@fernsroth.com>
2016-03-18 23:25:45 -0400

    debug uart working

--- firmware/project.mk
+++ firmware/project.mk
@@ -1,3 +1,9 @@
 USER_CFLAGS = 
-SRCS = src/main.c
+SRCS = \
+	src/main.c  \
+	libs/utils/debug.c \
+	libs/utils/syscalls.c \
+	libs/utils/time.c \
+	libs/utils/ringbufferdma.c \
+	libs/utils/utils.c
 SSRCS =
--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,30 +1,38 @@
 
-#include <pinout.h>
+#include "platform_config.h"
+#include <utils/debug.h>
+#include <utils/time.h>
+#include <string.h>
+
+#define DEBUG_OUT(format, ...) printf("%s:%d: " format, __FILE__, __LINE__, ##__VA_ARGS__)
+//#define DEBUG_OUT(format, ...)
 
 volatile GPIO_PinState lastState;
 
-void sleep_ms(uint32_t ms);
-void sleep_us(uint32_t us);
-
 void setup() {
+  debug_setup();
+  sleep_ms(100);
+  DEBUG_OUT("setup\n");
+  
   lastState = GPIO_PIN_RESET;
+  
+  DEBUG_OUT("setup complete\n");
+  DEBUG_OUT("> ");
 }
 
 void loop() {
+  debug_tick();
+  
   GPIO_PinState newState = lastState == GPIO_PIN_RESET ? GPIO_PIN_SET : GPIO_PIN_RESET;
   HAL_GPIO_WritePin(DEBUG_GPIO_PORT, DEBUG_GPIO_PIN, newState);
   lastState = newState;
   sleep_ms(500);
 }
 
-void sleep_ms(uint32_t ms) {
-  volatile uint32_t i;
-  for (i = ms; i != 0; i--) {
-    sleep_us(1000);
+void debug_processLine(const char* line) {
+  if (strlen(line) == 0) {
+  } else {
+    DEBUG_OUT("invalid debug command: %s\n", line);
   }
-}
-
-void sleep_us(uint32_t us) {
-  volatile uint32_t i;
-  for (i = ((SystemCoreClock / 8000000) * us); i != 0; i--) {}
+  DEBUG_OUT("> ");
 }
